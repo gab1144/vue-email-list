7a380aa65146eeccbc670bf72c84f319b5b92205
gab1144 <gabry.rinciari04@gmail.com>
2022-11-11 17:26:43 +0100

    [add] bonus

--- js/main.js
+++ js/main.js
@@ -7,8 +7,9 @@ createApp({
       title: "Email list",
       apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
       emailList: [],
-      isLoaded:false,
-      limit: 10
+      isLoaded:null,
+      limit: 10,
+      loading: false
     }
   },
   methods:{
@@ -18,9 +19,11 @@ createApp({
      for( let i=0; i < this.limit ; i++){
         axios.get(this.apiUrl)
           .then( result => {
+            this.loading = true;
             this.emailList.push(result.data.response);
             if(this.emailList.length === this.limit){
               this.isLoaded = true;
+              this.loading = false;
             }
           })
           .catch( error => {
